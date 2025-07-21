6f7f2e1a20bc6ff3dd165ada0a85ee21d43d45bf
benjamin31-05 <benjaminestebann31@gmail.com>
2025-07-20 20:22:01 -0500

    feat(SeleccionLocal): [HU-90]-119: Implementar selector de local en carrito

--- src/main/java/com/utp/controller/CarritoController.java
+++ src/main/java/com/utp/controller/CarritoController.java
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+//Implementar logica del local en el carrito
 @Controller
 public class CarritoController {
 
